12ae10dff2f4c2e9ab0d76838057167693c21b3a
David <charlesinvestingltd@gmail.com>
2020-03-12 14:47:34 -0400

    added some style if you know what I mean

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
-configure do
-    set :views, 'src/views'
-end
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
