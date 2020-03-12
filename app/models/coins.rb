fe80ec3662d6fc619422455815c668c12caec640
David <charlesinvestingltd@gmail.com>
2020-03-12 14:27:23 -0400

    established layout file

---
+++ app/models/coins.rb
@@ -0,0 +1,3 @@
+class Coins < ActiveRecord::Base
+  belongs_to :user
+end
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,9 @@
 require_relative 'boot'
 
 require 'rails/all'
+configure do
+    set :views, 'src/views'
+end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,6 +13,13 @@ module Coinfinder2
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    def logged_in?
+        !!current_user
+      end
+
+      def current_user
+        @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+      end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
